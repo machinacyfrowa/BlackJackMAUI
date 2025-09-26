5233540dabd9e3938fcddaa32eae05492790a3ba
Paweł Zawadzki <admin@machinacyfrowa.pl>
2025-09-26 09:42:44 +0200

    Zaktualizowano interfejs i dodano logikę kart
    W pliku `MainPage.xaml` usunięto obrazek i etykietę powitalną, a dodano nową etykietę z tekstem "tu bedzie wylosowana karta". Zmieniono przycisk na "Wyciągnij losową kartę" oraz jego funkcjonalność, aby wywoływał metodę `PullRandomCard`.
    
    W pliku `MainPage.xaml.cs` usunięto zmienną `count` oraz metodę `OnCounterClicked`, a dodano nową metodę `PullRandomCard`, która tworzy nową kartę i aktualizuje tekst etykiety `RandomCard`.
    
    W pliku `Card.cs` dodano definicje klas `Card`, `Suit` i `Rank`, które reprezentują karty do gry, w tym konstruktor losujący oraz metodę `ToString` zwracającą opis karty.

---
+++ BlackJackMAUI/Card.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackMAUI
+{
+    enum Suit
+    {
+        Hearts,
+        Diamonds,
+        Clubs,
+        Spades
+    }
+    enum Rank
+    {
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+    internal class Card
+    {
+        /// <summary>
+        /// Każda karta posiada rangę i kolor
+        /// Zostawiamy je publicznie dostępne dla wygody
+        /// TODO: Zmienić na prywatne?
+        /// </summary>
+        public Rank Rank;
+        public Suit Suit;
+        /// <summary>
+        /// Uproszczony konstruktor - zwraca losową kartę
+        /// </summary>
+        public Card()
+        {
+            //Tworzymy sobie generator liczb losowych
+            Random r = new Random();
+            //losujemy rangę z zakresu 0-12 (13 wartości)
+            int randomInt = r.Next(0, 13);
+            //rzutujemy liczbę losową na Enum - dostajemy losową rangę
+            Rank = (Rank)randomInt;
+            //losujemy kolor z zakresu 0-3 (4 wartości)
+            randomInt = r.Next(0, 4);
+            //rzutujemy liczbę losową na Enum - dostajemy losowy kolor
+            Suit = (Suit)randomInt;
+        }
+        /// <summary>
+        /// Domyślny konstruktor - tworzy kartę o podanej randze i kolorze
+        /// </summary>
+        /// <param name="rank">Ranga karty</param>
+        /// <param name="suit">Kolor karty</param>
+        public Card(Rank rank, Suit suit)
+        {
+            Rank = rank;
+            Suit = suit;
+        }
+        /// <summary>
+        /// Zwraca czytelny opis karty, np. "Ace of Spades"
+        /// </summary>
+        /// <returns>Opis karty</returns>
+        public override string ToString()
+        {
+            return Rank + " of " + Suit;
+        }
+    }
+}
