1801376472abd0421659ffc87f5a1514917aabdd
Paweł Zawadzki <admin@machinacyfrowa.pl>
2025-10-10 09:38:35 +0200

    beta interface

--- BlackJackMAUI/MainPage.xaml.cs
+++ BlackJackMAUI/MainPage.xaml.cs
@@ -17,30 +17,30 @@
         //    RandomCard.Text = card.ToString();
         //    CardImage.Source = ImageSource.FromFile(card.GetFileName());
         //}
-        private void TestHand(object sender, EventArgs e)
-        {
+        //private void TestHand(object sender, EventArgs e)
+        //{
 
-            //dodanie do ręki dwóch kart
-            PlayerHand.AddCard(Shoe.Draw());
-            PlayerHand.AddCard(Shoe.Draw());
-            //wykorzystamy sobie labelke od random card zeby wyświetlić wynik
-            RandomCard.Text = "Wartość kart: " + PlayerHand.Value();
-            //renderujemy karty na ekranie
-            RenderCards();
-        }
-        private void RenderCards()
-        {
-            //tutaj będziemy renderować karty na ekranie
-            //najpierw czyścimi stare karty
-            PlayerCardsHLayout.Children.Clear();
-            //reka gracza - PlayerCardsHLayout
-            foreach (Card card in PlayerHand.cards)
-            {
-                Image cardImage = new Image();
-                cardImage.Source = ImageSource.FromFile(card.GetFileName());
-                cardImage.HeightRequest = 150;   
-                PlayerCardsHLayout.Add(cardImage);
-            }
-        }
+        //    //dodanie do ręki dwóch kart
+        //    PlayerHand.AddCard(Shoe.Draw());
+        //    PlayerHand.AddCard(Shoe.Draw());
+        //    //wykorzystamy sobie labelke od random card zeby wyświetlić wynik
+        //    RandomCard.Text = "Wartość kart: " + PlayerHand.Value();
+        //    //renderujemy karty na ekranie
+        //    RenderCards();
+        //}
+        //private void RenderCards()
+        //{
+        //    //tutaj będziemy renderować karty na ekranie
+        //    //najpierw czyścimi stare karty
+        //    PlayerCardsHLayout.Children.Clear();
+        //    //reka gracza - PlayerCardsHLayout
+        //    foreach (Card card in PlayerHand.cards)
+        //    {
+        //        Image cardImage = new Image();
+        //        cardImage.Source = ImageSource.FromFile(card.GetFileName());
+        //        cardImage.HeightRequest = 150;   
+        //        PlayerCardsHLayout.Add(cardImage);
+        //    }
+        //}
     }
 }
